0a9195b93e49ba3092952986c5408f12ab00e1e7
Ayush Kumar <ayush9162365835@gmail.com>
2021-06-16 17:08:28 +0530

    initial commit

---
+++ public/index.js
@@ -0,0 +1,57 @@
+
+
+//document.querySelectorAll(.btnn).style.display="block";
+function myFunction1() {
+//  var x = document.getElementById("myDIV1");
+var x=$("#myDIV1");
+  if (x.css("display") === "none")
+ {
+      x.css( "display","block");
+  }
+  else
+  {
+        x.css( "display","none");
+  }
+}
+
+function myFunction2() {
+  var x=$("#myDIV2");
+    if (x.css("display") === "none")
+   {
+        x.css( "display","block");
+    }
+    else
+    {
+          x.css( "display","none");
+    }
+}
+
+
+function myFunction3() {
+  var x=$("#myDIV3");
+    if (x.css("display") === "none")
+   {
+        x.css( "display","block");
+    }
+    else
+    {
+          x.css( "display","none");
+    }
+}
+
+
+
+
+function myFunction4() {
+  var x=$("#myDIV4");
+    if (x.css("display") === "none")
+   {
+        x.css( "display","block");
+    }
+    else
+    {
+          x.css( "display","none");
+    }
+}
+
+document.querySelector(".sum").style.fontSize="20px";
